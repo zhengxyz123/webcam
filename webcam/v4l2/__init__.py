f38683f56eb6ba818b5e50404bdc0d1c185c9b10
zhengxyz123 <3422794023@qq.com>
2024-04-29 21:42:17 +0800

    Fix a bug.

--- webcam/v4l2/__init__.py
+++ webcam/v4l2/__init__.py
@@ -126,22 +126,23 @@ class v4l2WebCam(BaseWebCam):
                 break
             self._available_pixfmt.append(vfmt.pixelformat)
 
-        if any([fmt in self._available_pixfmt for fmt in pixfmt_rgb]):
+    def _init(self):
+        vfmt = v4l2_format(type=v4l2_buf_type.V4L2_BUF_TYPE_VIDEO_CAPTURE)
+        vfmt.fmt.pix.width = self._size[0]
+        vfmt.fmt.pix.height = self._size[1]
+        if any(l := [fmt in self._available_pixfmt for fmt in pixfmt_rgb]):
             self._data_fmt = "RGB"
-        elif any([fmt in self._available_pixfmt for fmt in pixfmt_rgba]):
+            vfmt.fmt.pix.pixelformat = pixfmt_rgb[l.index(True)]
+        elif any(l := [fmt in self._available_pixfmt for fmt in pixfmt_rgba]):
             self._data_fmt = "RGBA"
+            vfmt.fmt.pix.pixelformat = pixfmt_rgba[l.index(True)]
         elif V4L2_PIX_FMT_MJPEG in self._available_pixfmt:
             self._data_fmt = "MJPEG"
+            vfmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG
         else:
             raise WebCamException(
                 f"{self._device} does not support RGB, RGBA or MJPEG format"
             )
-
-    def _init(self):
-        vfmt = v4l2_format(type=v4l2_buf_type.V4L2_BUF_TYPE_VIDEO_CAPTURE)
-        vfmt.fmt.pix.width = 640
-        vfmt.fmt.pix.height = 480
-        vfmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG
         VIDIOC_S_FMT(self._fd, vfmt)
         self._size = (vfmt.fmt.pix.width, vfmt.fmt.pix.height)
 
