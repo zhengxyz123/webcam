b071b8277f9ef0591afc601b63ba1b0cf8e43c39
zhengxyz123 <3422794023@qq.com>
2024-06-27 09:44:46 +0800

    Update fourcc generator.

--- gen_format_desc.py
+++ gen_format_desc.py
@@ -3,20 +3,18 @@ import linecache
 videodev2_h = "/usr/include/linux/videodev2.h"
 now_line = 512
 
-
 while True:
     line = linecache.getline(videodev2_h, now_line).strip()
     now_line += 1
-    if line.startswith("/*") and line.endswith("*/"):
-        continue
-    elif len(line) == 0:
-        continue
-    elif "v4l2_fourcc" not in line:
+    if line.startswith("#define") and "v4l2_fourcc" in line:
+        pass
+    elif "struct" in line:
         break
+    else:
+        continue
 
     if "/*" in line:
-        line = line[: line.find("/*")]
-    line = line.replace("\t", " ")
+        line = line.replace("\t", " ")[: line.find("/*")]
     _, name, value = line.split(" ", 2)
     value = value.replace("'", '"').strip()
     print(f"{name} = {value}")
